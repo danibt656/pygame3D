b15b8e51a151295f84ed95046aa9dc93fdd3e3db
danibt656 <dbmartin656@gmail.com>
2022-01-30 18:39:35 +0100

    replaced movement and rotation for matrices

--- wireframe.py
+++ wireframe.py
@@ -2,6 +2,7 @@
 # Wireframe class
 #
 import numpy as np
+from numpy.linalg import multi_dot
 import math
 
 
@@ -70,7 +71,7 @@ class Wireframe:
     def addEdges(self, edge_list):
         self.edges += edge_list
     
-    # For debugging
+    ## For debugging ##
     def outputNodes(self):
         print("\n --- Nodes ---")
         for i, (x, y, z, _) in enumerate(self.nodes):
@@ -80,6 +81,7 @@ class Wireframe:
         print("\n --- Edges ---")
         for i, (node1, node2) in enumerate(self.edges):
             print(f" {i}: {node1} -> {node2}")
+    ##               ##
 
     def transform(self, matrix):
         """
@@ -87,71 +89,72 @@ class Wireframe:
         """
         self.nodes = np.dot(self.nodes, matrix)
 
-    def translate(self, axis, d):
-        """
-        Translate each node of a wireframe by d along given axis
-        """
-        if axis in ['x', 'y', 'z']:
-            for node in self.nodes:
-                setattr(node, axis, getattr(node, axis) + d)
+    def scale(self, scaleMatrix):
+        center = np.array(self.findCenter())
+        centerInv = center * -1
 
-    def scale(self, centre_x, centre_y, scale):
-        """
-        Scale the wireframe from the centre of the screen
-        """
-        for node in self.nodes:
-            node.x = centre_x + scale * (node.x - centre_x)
-            node.y = centre_y + scale * (node.y - centre_y)
-            node.z *= scale
+        tf_center = translationMatrix(*center)
+        tf_centerInv = translationMatrix(*centerInv)
 
-    def rotateX(self, cx, cy, cz, radians):
-        for node in self.nodes:
-            y = node.y - cy
-            z = node.z - cz
-            d = math.hypot(y, z)
-            theta = math.atan2(y, z) + radians
-            node.z = cz + d * math.cos(theta)
-            node.y = cy + d * math.sin(theta)
+        tf_scale = multi_dot([tf_centerInv, scaleMatrix, tf_center])
+        self.nodes = np.dot(self.nodes, tf_scale)
 
-    def rotateY(self, cx, cy, cz, radians):
-        for node in self.nodes:
-            x = node.x - cx
-            z = node.z - cz
-            d = math.hypot(x, z)
-            theta = math.atan2(x, z) + radians
-            node.z = cz + d * math.cos(theta)
-            node.x = cx + d * math.sin(theta)
+    def rotateX(self, rotateXMatrix):
+        center = np.array(self.findCenter())
+        centerInv = center * -1
 
-    def rotateZ(self, cx, cy, cz, radians):
-        for node in self.nodes:
-            x = node.x - cx
-            y = node.y - cy
-            d = math.hypot(y, x)
-            theta = math.atan2(y, x) + radians
-            node.x = cx + d * math.cos(theta)
-            node.y = cy + d * math.sin(theta)
+        tf_center = translationMatrix(*center)
+        tf_centerInv = translationMatrix(*centerInv)
+
+        tf_rotate = multi_dot([tf_centerInv, rotateXMatrix, tf_center])
+        self.nodes = np.dot(self.nodes, tf_rotate)
+
+    def rotateY(self, rotateYMatrix):
+        center = np.array(self.findCenter())
+        centerInv = center * -1
+
+        tf_center = translationMatrix(*center)
+        tf_centerInv = translationMatrix(*centerInv)
+
+        tf_rotate = multi_dot([tf_centerInv, rotateYMatrix, tf_center])
+        self.nodes = np.dot(self.nodes, tf_rotate)
+    
+    def rotateZ(self, rotateZMatrix):
+        center = np.array(self.findCenter())
+        centerInv = center * -1
+
+        tf_center = translationMatrix(*center)
+        tf_centerInv = translationMatrix(*centerInv)
+
+        tf_rotate = multi_dot([tf_centerInv, rotateZMatrix, tf_center])
+        self.nodes = np.dot(self.nodes, tf_rotate)
 
     def findCenter(self):
         """
         Find center of the wireframe
         """
         num_nodes = len(self.nodes)
-        meanX = sum([node.x for node in self.nodes]) / num_nodes
-        meanY = sum([node.y for node in self.nodes]) / num_nodes
-        meanZ = sum([node.z for node in self.nodes]) / num_nodes
+        meanX = sum([node[0] for node in self.nodes]) / num_nodes
+        meanY = sum([node[1] for node in self.nodes]) / num_nodes
+        meanZ = sum([node[2] for node in self.nodes]) / num_nodes
 
         return (meanX, meanY, meanZ)
 
 
 
 if __name__ == "__main__":
+    """
+    Quick debug function
+    """
     cube = Wireframe()
+
     cube_nodes = [(x, y, z) for x in (0, 1) for y in (0, 1) for z in (0, 1)]
+
     cube.addNodes(np.array(cube_nodes))
     cube.addEdges([(n, n + 4) for n in range(0, 4)])
     cube.addEdges([(n, n + 1) for n in range(0, 8, 2)])
     cube.addEdges([(n, n + 2) for n in (0, 1, 4, 5)])
+
     cube.outputNodes()
     cube.outputEdges()
-
 
--- wireframe_display.py
+++ wireframe_display.py
@@ -7,10 +7,10 @@ KEY_TO_FUNCTION = {
     # Movement
     pg.K_a: (lambda x: x.translateAll([-10, 0, 0])),
     pg.K_d: (lambda x: x.translateAll([10, 0, 0])),
-    pg.K_LSHIFT: (lambda x: x.translateAll([0, 10, 0])),
-    pg.K_SPACE: (lambda x: x.translateAll([0, -10, 0])),
-    pg.K_w: (lambda x: x.scaleAll(1.25)),
-    pg.K_s: (lambda x: x.scaleAll(0.8)),
+    pg.K_SPACE: (lambda x: x.translateAll([0, 10, 0])),
+    pg.K_LSHIFT: (lambda x: x.translateAll([0, -10, 0])),
+    pg.K_w: (lambda x: x.scaleAll([1.25, 1.25, 1.25])),
+    pg.K_s: (lambda x: x.scaleAll([0.8, 0.8, 0.8])),
 
     # Rotation
     pg.K_q: (lambda x: x.rotateAll('X',  0.1)),
@@ -36,9 +36,9 @@ class ProjectionViewer:
         self.wireframes = {}
         self.displayNodes = True
         self.displayEdges = True
-        self.nodeColor = (255, 255, 255)
+        self.nodeColor = (255, 0, 0)
         self.edgeColor = (200, 200, 200)
-        self.nodeRadius = 4
+        self.nodeRadius = 2
 
     def run(self):
         """
@@ -97,33 +97,49 @@ class ProjectionViewer:
         """
         Scale all wireframes by a given scale, centered on screen's center
         """
-        centre_x = self.width / 2
-        centre_y = self.height / 2
-
         matrix = wf.scaleMatrix(*scale)
+
         for wireframe in self.wireframes.values():
-            wireframe.scale(centre_x, centre_y, scale)
+            wireframe.scale(matrix)
 
     def rotateAll(self, axis, theta):
         """
         Rotate all wireframes about their centre, along given axis by given angle
         """
         rotateFunction = 'rotate' + axis.upper()
+        rotateMatrix = []
+
+        if axis == 'X':
+            rotateMatrix = wf.rotateXMatrix(theta)
+        elif axis == 'Y':
+            rotateMatrix = wf.rotateYMatrix(theta)
+        elif axis == 'Z':
+            rotateMatrix = wf.rotateZMatrix(theta)
 
         for wireframe in self.wireframes.values():
-            center = wireframe.findCenter()
-            getattr(wireframe, rotateFunction)(*center, theta)
+            getattr(wireframe, rotateFunction)(rotateMatrix)
+
+# End of ProjectionViewer
 
 
+def _setup_cube(ix, iy, iz, size):
+    """
+    Set up a cube of 'size' size on origin (ix, iy, iz)
+    """
 
-if __name__ == '__main__':
     cube = wf.Wireframe()
-    cube_nodes = [(x, y, z) for x in (50,250) for y in (50,250) for z in (50,250)]
+    cube_nodes = [(x, y, z) for x in (ix,ix+size) for y in (iy,iy+size) for z in (iz,iz+size)]
     cube.addNodes(np.array(cube_nodes))
     cube.addEdges([(n,n+4) for n in range(0,4)] + \
                   [(n,n+1) for n in range(0,8,2)] + \
                   [(n,n+2) for n in (0,1,4,5)])
+    return cube
+
+if __name__ == '__main__':
+    cube1 = _setup_cube(50, 50, 50, 30)
+    cube2 = _setup_cube(80, 50, 50, 30)
     
-    pv = ProjectionViewer(400, 300)
-    pv.addWireframe('cube', cube)
+    pv = ProjectionViewer(800, 600)
+    pv.addWireframe('cube1', cube1)
+    pv.addWireframe('cube2', cube2)
     pv.run()
